f48e70cdc601656db0e2cb05ede154af7ec8f37e
Korei Klein <korei.klein@gmail.com>
2013-02-28 15:56:53 -0500

    Implement basic slideshow functionality for lists of enriched formulas.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # Copyright (C) 2013 Korei Klein <korei.klein1@gmail.com>
 
-from examples.easy_induction import ending_claim as claim
+from examples.easy_induction import transition, ending_claim as claim
 from extraction.python.examples import easy_induction, silly
-from ui.run.glimmediate import static as static_gl_image
+from ui.run.glimmediate import slideshow, static as static_gl_image
 
 from sys import setrecursionlimit
 
@@ -11,4 +11,5 @@ setrecursionlimit(500000)
 silly.test()
 easy_induction.test()
 
-static_gl_image.run(claim)
+#static_gl_image.run(claim)
+slideshow.run([t.tgt() for t in transition.values()])
---
+++ ui/run/glimmediate/slideshow.py
@@ -0,0 +1,58 @@
+# Copyright (C) 2013 Korei Klein <korei.klein1@gmail.com>
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+
+from ui.render.gl.render_immediate import render
+
+from stack import gl
+
+import random
+
+currentStack = None
+remainingClaims = None
+
+w = 1340
+h = 680
+
+scale = 0.14
+
+def display():
+  global currentStack
+  glClear(GL_COLOR_BUFFER_BIT)
+  currentStack._backend.render()
+  glutSwapBuffers()
+
+def keyboard(key, x, y):
+  global currentStack
+  global remainingClaims
+  remainingClaims.pop(0)
+  if key == 'q' or len(remainingClaims) == 0:
+    exit()
+  else:
+    currentStack = render(remainingClaims[0])
+    glutPostRedisplay()
+
+def run(claims):
+  if len(claims) == 0:
+    print "No claims to render in sildeshow."
+    return
+
+  global currentStack
+  global remainingClaims
+  glutInit()
+  glutInitWindowSize(w, h)
+  glutCreateWindow("Demo")
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA)
+  glutDisplayFunc(display)
+  glutKeyboardFunc(keyboard)
+  glClearColor(1.0,1.0,1.0,0.0)
+  glColor3f(0.0,0.0, 0.0)
+  glMatrixMode(GL_PROJECTION)
+  glLoadIdentity()
+  gluOrtho2D(0.0, w + 0.0, 0.0, h + 0.0)
+  glScale(scale, scale, scale)
+  currentStack = render(claims[0])
+  remainingClaims = claims
+  glutMainLoop()
