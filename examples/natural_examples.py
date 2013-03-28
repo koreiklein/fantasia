36712a02a39a9140721c0fffdec8155247d01370
Korei Klein <korei.klein@gmail.com>
2013-03-28 15:36:00 -0400

    Add the example file used to test the use of Always.

---
+++ examples/natural_examples.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2013 Korei Klein <korei.klein1@gmail.com>
+
+from calculus import enriched
+from lib import natural
+
+import importables
+import about
+
+transition = natural.preludeFormula.identity().forwardFollow(lambda x:
+    x.forwardSingleton(enriched.andType).forwardFollow(lambda x:
+      x.forwardAssociateOut(0, 0)))
+transition = transition.forwardFollow(lambda x:
+    importables.beginImportingOnIthFollow(x, 0, lambda x:
+      importables.finishImporting(x,
+        about.about([natural.zero], 0)))).forwardFollow(lambda x:
+            x.forwardHeavyClean())
+
+transition.translate()
