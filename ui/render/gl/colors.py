c7ef169f6ecfe9d09aae3ec14bdfd459b0f33291
Korei Klein <korei.klein@gmail.com>
2013-04-01 16:53:46 -0400

    Change how colors are chosen for dividers.

--- ui/render/gl/colors.py
+++ ui/render/gl/colors.py
@@ -5,11 +5,17 @@ from OpenGL.GL import *
 
 from ui.stack.color import Color
 from calculus import enriched
+from calculus.basic import andType, orType
 
 trueDividerColor = Color(r = 0.0, g = 0.0, b = 0.0)
 falseDividerColor = Color(r = 0.0, g = 0.0, b = 0.0)
-concreteDividerColor = Color(r = 0.0, g = 0.0, b = 1.0)
-demorganedDividerColor = Color(r = 0.68, g = 0.09, b = 0.63)
+
+# Prefer choosing the colors of dividers based on the underlying basic type.
+#concreteDividerColor = Color(r = 0.0, g = 0.0, b = 1.0)
+#demorganedDividerColor = Color(r = 0.68, g = 0.09, b = 0.63)
+andBasedDividerColor = Color(r = 0.0, g = 0.0, b = 1.0)
+orBasedDividerColor = Color(r = 0.68, g = 0.09, b = 0.63)
+
 quantifierDividerColor = Color(r = 0.5, g = 0.5, b = 0.5)
 alwaysBackgroundColor = Color(r = 0.0, g = 1.0, b = 1.0, a = 0.2)
 maybeBackgroundColor = Color(r = 1.0, g = 0.5, b = 0.0, a = 0.8)
@@ -19,10 +25,12 @@ textColor = Color(r = 0.2, g = 0.2, b = 0.2, a= 1.0)
 variableColor = Color(r = 0.2, g = 0.5, b = 0.2, a= 1.0)
 
 def colorOfConjType(type):
-  if type in enriched.concreteConjTypes:
-    return concreteDividerColor
+  basicType = enriched.correspondingConcreteBasicType(type)
+  if basicType == andType:
+    return andBasedDividerColor
   else:
-    return demorganedDividerColor
+    assert(basicType == orType)
+    return orBasedDividerColor
 
 def colorOfUnitType(type):
   if type in [enriched.andType, enriched.withType]:
