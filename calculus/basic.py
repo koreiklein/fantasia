2542a16110410d86f2b2479ac8a2d405f92db21d
Korei Klein <korei.klein@gmail.com>
2013-06-20 14:00:51 -0400

    Fix typo and change forwardCopy to always do an update.

--- calculus/basic.py
+++ calculus/basic.py
@@ -238,7 +238,7 @@ class Exists(Object):
     return OnBody(self.variable, self.value.simplify())
 
   def __eq__(self, other):
-    if self.__class__n != other.__class__:
+    if self.__class__ != other.__class__:
       return False
     else:
       if self.variable == other.variable:
@@ -691,19 +691,16 @@ class Always(Object):
   #   X is in f(B)
   #   B == Always(C) for some C
   def produceFiltered(self, f):
-    new = self.updateVariables()
     result = []
     result.extend([(self.forwardOnAlways(a).forwardFollow(lambda x:
       x.forwardDistributeAlways().forwardFollow(lambda x:
         x.forwardOnLeftFollow(lambda x:
           x.forwardUnalways()))), X) for a, X in self.value.produceFiltered(f)])
-    result.extend([(self.forwardCopy(new), X) for X in f(new)])
+    result.extend([(self.forwardCopy(), X) for X in f(self)])
     return result
 
-  def forwardCopy(self, new = None):
-    if new is None:
-      new = self
-    return Copy(src = self, tgt = And(new, self))
+  def forwardCopy(self):
+    return Copy(src = self, tgt = And(self.updateVariables(), self))
 
   def __eq__(self, other):
     return self.__class__ == other.__class__ and self.value == other.value
